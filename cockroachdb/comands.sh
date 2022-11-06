88c179b63a22a43573f9769c12beeef552921a5c
Kenneth Campos Campos <kcamposc@ice.go.cr>
2022-11-06 00:25:49 -0600

    Agregar detalles para probar CockroachDB en minikube

---
+++ cockroachdb/comands.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+alias mk="minikube kubectl --"   
+minikube start  --kubernetes-version=v1.23.12 --driver=virtualbox --memory=8192 --cpus 4 --addons="metrics-server,metallb"
+minikube dashboard
+#
+mk apply -f cockroachdb-statefulset-insecure.yaml    
+
+#Listar pods
+mk get pods
+
+#Cluster init
+mk create -f https://raw.githubusercontent.com/cockroachdb/cockroach/master/cloud/kubernetes/cluster-init.yaml
+
+
+mk scale -n default statefulset cockroachdb --replicas=5
+
+#Iniciliazar el workload
+mk run workload-init -it --image=cockroachdb/cockroach --rm --restart=Never -- workload init bank \
+'postgresql://root@cockroachdb-public:26257?sslmode=disable' 
+
+#Lanzar el workload por 5 minutos
+mk run workload-init -it --image=cockroachdb/cockroach --rm --restart=Never -- workload run bank --duration=5m \
+'postgresql://root@cockroachdb-public:26257?sslmode=disable' 
+
+minikube delete
